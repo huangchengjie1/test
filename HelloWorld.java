cacf879b03c8f7d1bac5eeff6ca237b25d6f775c
lxh <lixiaohua@itcast.cn>
2019-04-30 19:42:59 +0800

    第三次修改

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+ */
+public class HelloWorld {
+  //  大家思考：如何解决下面的连接数据库代码与数据库驱动代码耦合问题？
+
+    public static void main(String[] args) throws Exception{
+       
+	  System.out.private("11111111111");
+	  System.out.private("22222222222")
+    }
+}
